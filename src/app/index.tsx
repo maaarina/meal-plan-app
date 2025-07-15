4c05b3ac8fbd8bcdc141d813e10ed1c033961434
Marina Gabrielly <marina.gabri3lly@gmail.com>
2025-07-15 20:43:24 -0300

    subindo pastas

---
+++ src/app/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { View, Text, Button, StyleSheet, ScrollView } from "react-native";
+import { styles } from "./style"
+
+export default function Index(){
+
+    return(
+        <View>
+            <Text style= {styles.title} > Meal Plan</Text>
+
+            <View>
+                
+            </View>
+        </View>
+    )
+} 
---
+++ src/app/style.ts
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const styles = StyleSheet.create({
+    title: {
+        fontSize: 20,
+        fontWeight: "bold",
+        fontFamily: "TimesNewRoman",
+        justifyContent: "center",
+        alignItems: "center",
+        padding: 15, 
+    }
+})
